f63fbdba1261c90d6c579fa75cc08a1b7065ce7f
Jenny Wu <jennywuzheng@gmail.com>
2022-11-29 22:06:35 -0500

    socket

--- client/index.js
+++ client/index.js
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux';
 import store from './app/store.js';
 import App from './app/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import socket from 'socket.io-client';
+const clientSocket = socket(window.location.origin);
+clientSocket.on('connect', () => { 
+  console.log('Connected to server'); 
+})
 
 const root = createRoot(document.getElementById('app'));
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const { db } = require('./db')
 const PORT = process.env.PORT || 8080
 const app = require('./app')
 const seed = require('../script/seed');
+const socket = require('socket.io')
 
 const init = async () => {
   try {
@@ -12,7 +13,11 @@ const init = async () => {
       await db.sync()
     }
     // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+    const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+    const serverSocket = socket(server)
+    serverSocket.on('connection', (socket)=>{
+      console.log(`Connected from client ${socket.id}`)
+    })
   } catch (ex) {
     console.log(ex)
   }
